c39afdbfb0c38c8a2b9836d92907130d393187af
Josh Lewis <joshua.lewis@skybettingandgaming.com>
2020-11-21 14:37:09 +0000

    Update tests to account for change to inventory add readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # price-basket
+
+This application can be packaged using SBT
+The reference manual for SBT can be found [here](https://www.scala-sbt.org/1.x/docs/index.html)
--- src/test/scala/com/example/inventory/BasicInventoryTest.scala
+++ src/test/scala/com/example/inventory/BasicInventoryTest.scala
@@ -4,23 +4,23 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class BasicInventoryTest extends AnyWordSpec {
 
-  val apple: ShopItem = ShopItem(1L, "apple", 0.35)
+  val apple: ShopItem = ShopItem(4L, "apples", 1.00)
 
   "Basic Inventory" should {
     "load apples from description" in {
 
       val inv = new BasicInventory
-      val searchTerms = Seq("Apple", "apple", "aPPLe")
+      val searchTerms = Seq("Apples", "apples", "aPPLes")
 
-      searchTerms.map(term => assert(inv.getItemCodeFromDescription(term) === Right(apple)))
+      searchTerms.map(term => assert(inv.getItemCodeFromDescription(term) === Right(4L)))
     }
 
     "load apples from item code" in {
 
       val inv = new BasicInventory
-      val result = inv.getShopItemFromID(1L)
+      val result = inv.getShopItemFromID(4L)
 
-      assert(result === Right(apple))
+      assert(result === apple)
 
     }
   }
