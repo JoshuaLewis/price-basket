eddd1d0fa717f654fd494c5f7c104528cc4d835d
Josh Lewis <joshua.lewis@skybettingandgaming.com>
2020-11-22 18:40:20 +0000

    unicode pound for windows - fixed

--- src/main/scala/com/example/checkout/Receipt.scala
+++ src/main/scala/com/example/checkout/Receipt.scala
@@ -5,9 +5,9 @@ import com.example.inventory.ShopItem
 case class Receipt(item: ShopItem, subTotal: BigDecimal, discount: (String, BigDecimal), total: BigDecimal){
   override def toString: String =
     s"""
-       |${item.description} (\u00A34${item.unitPrice.setScale(2, BigDecimal.RoundingMode.HALF_EVEN)} each)
-       |Subtotal:\u00A34${subTotal.setScale(2, BigDecimal.RoundingMode.HALF_EVEN)}
+       |${item.description} (\u00A3${item.unitPrice.setScale(2, BigDecimal.RoundingMode.HALF_EVEN)} each)
+       |Subtotal:\u00A3${subTotal.setScale(2, BigDecimal.RoundingMode.HALF_EVEN)}
        |${discount._1}
-       |Total price \u00A34${total.setScale(2, BigDecimal.RoundingMode.HALF_EVEN)}
+       |Total price \u00A3${total.setScale(2, BigDecimal.RoundingMode.HALF_EVEN)}
        |""".stripMargin
 }
